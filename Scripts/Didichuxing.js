af4998fc6cbe686a0384d5826ae8823f42cb6e9b
johnmillzer <johnmillzer@gmail.com>
2024-09-21 20:39:39 +0800

    Update Didichuxing.js

--- Scripts/Didichuxing.js
+++ Scripts/Didichuxing.js
@@ -28,7 +28,7 @@ if (url.includes("/other/pGetSceneList")) {
 if (url.includes("/homepage/v1/core")) {
   const keepNavIds = [];
   if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
-    obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
+    obj.data.order_cards.nav_list_card.data = [];  // 直接设置为空数组，移除所有导航栏项目
   }
   const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
   if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
