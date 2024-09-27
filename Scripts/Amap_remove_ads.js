6e75ce325eff71ed6e4514e4768ea9ddd0817ccc
johnmillzer <johnmillzer@gmail.com>
2024-09-27 12:58:47 +0800

    Update Amap_remove_ads.js

--- Scripts/Amap_remove_ads.js
+++ Scripts/Amap_remove_ads.js
@@ -170,7 +170,7 @@ if (url.includes("/aos/perception/publicTravel/beforeNavi")) {
   }
 } else if (url.includes("/shield/dsp/profile/index/nodefaasv3")) {
   // 我的页面
-  if (obj?.data?.cardList?.length > 0) {
+//  if (obj?.data?.cardList?.length > 0) {
   //  obj.data.cardList = obj.data.cardList.filter((i) => i?.dataKey === "MyOrderCard");
  // }
  // if (obj?.data?.tipData) {
