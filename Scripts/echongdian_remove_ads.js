86101c0bfca8f3c6c315731b32db40b71d5a5723
johnmillzer <johnmillzer@gmail.com>
2024-09-29 23:01:33 +0800

    Update echongdian_remove_ads.js

--- Scripts/echongdian_remove_ads.js
+++ Scripts/echongdian_remove_ads.js
@@ -2,21 +2,21 @@
 /*
 引用地址 https://raw.githubusercontent.com/johnmillzer/tool/refs/heads/main/Scripts/echongdian_remove_ads.js
 */
-// 2024-09-29 16:39:43
+// 2024-09-29 22:33:17
 
 const url = $request.url;
 if (!$response.body) $done({});
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/homepage/v1/core")) {
-  const keepNavIds = ['dache_anycar', 'driverservice', 'bike'];
-  if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
-    obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
-  }
-  const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
-  if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
-    obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
+if (url.includes("/cms_img")) {
+  // 开屏广告
+  if (obj?.data?.ad?.length > 0) {
+    for (let item of obj.data.ad) {
+      item.set.setting.display_time = 0;
+      item.creative[0].start_time = 3818332800; // Unix 时间戳 2090-12-31 00:00:00
+      item.creative[0].end_time = 3818419199; // Unix 时间戳 2090-12-31 23:59:59
+    }
   }
 }
 
