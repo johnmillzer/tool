4255c38df4864f895af469c9814b265c573aa204
johnmillzer <johnmillzer@gmail.com>
2024-09-29 22:33:03 +0800

    Update echongdian_remove_ads.js

--- Scripts/echongdian_remove_ads.js
+++ Scripts/echongdian_remove_ads.js
@@ -1,6 +1,6 @@
 
 /*
-引用地址 https://raw.githubusercontent.com/johnmillzer/tool/refs/heads/main/Scripts/Umetrip_remove_ads_plus_dition.js
+引用地址 https://raw.githubusercontent.com/johnmillzer/tool/refs/heads/main/Scripts/echongdian_remove_ads.js
 */
 // 2024-09-29 16:39:43
 
