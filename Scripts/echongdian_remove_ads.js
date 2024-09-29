fab6fb878332cfa9f5eb462c952ec5bedac79b04
johnmillzer <johnmillzer@gmail.com>
2024-09-29 22:32:45 +0800

    Update echongdian_remove_ads.js

--- Scripts/echongdian_remove_ads.js
+++ Scripts/echongdian_remove_ads.js
@@ -1,1 +1,24 @@
 
+/*
+引用地址 https://raw.githubusercontent.com/johnmillzer/tool/refs/heads/main/Scripts/Umetrip_remove_ads_plus_dition.js
+*/
+// 2024-09-29 16:39:43
+
+const url = $request.url;
+if (!$response.body) $done({});
+
+let obj = JSON.parse($response.body);
+
+if (url.includes("/homepage/v1/core")) {
+  const keepNavIds = ['dache_anycar', 'driverservice', 'bike'];
+  if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
+    obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
+  }
+  const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
+  if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
+    obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
+  }
+}
+
+
+$done({ body: JSON.stringify(obj) });
