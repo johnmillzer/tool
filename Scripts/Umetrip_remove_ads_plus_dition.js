57e2494107960cdee6990548024891dd31cc133b
johnmillzer <johnmillzer@gmail.com>
2024-09-30 23:10:45 +0800

    Update Umetrip_remove_ads_plus_dition.js

--- Scripts/Umetrip_remove_ads_plus_dition.js
+++ Scripts/Umetrip_remove_ads_plus_dition.js
@@ -9,14 +9,9 @@ if (!$response.body) $done({});
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/homepage/v1/core")) {
-  const keepNavIds = ['dache_anycar', 'driverservice', 'bike'];
-  if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
-    obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
-  }
-  const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
-  if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
-    obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
+if (url.includes("/ngstatic/static/fourthIndex")) {
+  if (obj && obj.adDesc) {
+    obj.adDesc = {};
   }
 }
 
