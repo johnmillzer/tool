8970c6ebda4cb187d4b01a0b7074e5ae4c9498a4
johnmillzer <johnmillzer@gmail.com>
2024-09-30 09:03:16 +0800

    Update echongdian_remove_ads.js

--- Scripts/echongdian_remove_ads.js
+++ Scripts/echongdian_remove_ads.js
@@ -9,16 +9,20 @@ if (!$response.body) $done({});
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/cms_img")) {
-  // 开屏广告
-  if (obj?.data?.ad?.length > 0) {
-    for (let item of obj.data.ad) {
-      item.set.setting.display_time = 0;
-      item.creative[0].start_time = 3818332800; // Unix 时间戳 2090-12-31 00:00:00
-      item.creative[0].end_time = 3818419199; // Unix 时间戳 2090-12-31 23:59:59
-    }
+if (url.includes("/cms_img/")) {
+  // 获取开屏广告
+  if (obj?.config?.backgroud?.type === "image") {
+    // 将图片显示时间设置为0，并设置过期时间
+    obj.config.expireTime = 3818419199000; // Unix时间戳 2090-12-31 23:59:59
+    obj.config.featureTime = 3818419199000; // Unix时间戳 2090-12-31 23:59:59
+    obj.config.backgroud.imageUrl = ""; // 移除图片链接
+  }
+
+  // 移除点击跳转
+  if (obj?.config?.click?.type === "url") {
+    obj.config.click.type = ""; // 清空点击行为
+    obj.config.click.webViewContent.uri = ""; // 移除跳转链接
   }
 }
-
 
 $done({ body: JSON.stringify(obj) });
