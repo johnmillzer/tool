eb38dfbec3af13142eed54d062352aabb9ba4e20
johnmillzer <johnmillzer@gmail.com>
2024-09-30 23:13:56 +0800

    Update Umetrip_remove_ads_plus_dition.js

--- Scripts/Umetrip_remove_ads_plus_dition.js
+++ Scripts/Umetrip_remove_ads_plus_dition.js
@@ -2,14 +2,14 @@
 /*
 引用地址 https://raw.githubusercontent.com/johnmillzer/tool/refs/heads/main/Scripts/Umetrip_remove_ads_plus_dition.js
 */
-// 2024-09-29 16:39:43
+// 2024-09-30 23:13:51
 
 const url = $request.url;
 if (!$response.body) $done({});
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/ngstatic/static/fourthIndex")) {
+if (url.includes("/gateway/api/umetrip/native")) {
   if (obj && obj.adDesc) {
     obj.adDesc = {};
   }
