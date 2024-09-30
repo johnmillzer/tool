0be250deeb950256579252f7d4954ce4d7e625e0
johnmillzer <johnmillzer@gmail.com>
2024-09-30 21:04:38 +0800

    Update qdmetro_remove_ads.js

--- Scripts/qdmetro_remove_ads.js
+++ Scripts/qdmetro_remove_ads.js
@@ -7,8 +7,8 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("ngstatic/appScreenAds/requestAds")) {
-  if (obj && obj.data && obj.data.showTime) {
-    obj.data.showTime = "null";
+  if (obj && obj.data && obj.data.duration) {
+    obj.data.duration = "0";
   }
 }
 
