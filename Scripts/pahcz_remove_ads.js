747d22357e9002880b8a7d11ac39778d67674f78
johnmillzer <johnmillzer@gmail.com>
2024-10-20 15:36:25 +0800

    Create pahcz_remove_ads.js

---
+++ Scripts/pahcz_remove_ads.js
@@ -0,0 +1,15 @@
+// 2024-10-20 15:24:05
+// 平安好车主去广告
+
+const url = $request.url;
+if (!$response.body) $done({});
+
+let obj = JSON.parse($response.body);
+
+if (url.includes("download/bpgbd-sys-sf-prd/")) {
+  if (obj && obj.data) {
+    obj.data.fingernailBgColor = "0,0,0";
+  }
+}
+
+$done({ body: JSON.stringify(obj) });
